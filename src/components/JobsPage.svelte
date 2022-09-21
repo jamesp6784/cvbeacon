13cbb60e6c25a9499811b696d962023bd45bb002
jamesp6784 <105037949+jamesp6784@users.noreply.github.com>
2022-09-21 21:09:36 +1200

    Make buttons nicer

--- src/components/JobsPage.svelte
+++ src/components/JobsPage.svelte
@@ -2,6 +2,7 @@
     import BasePage from "./BasePage.svelte";
     import JobListing from "./widgets/JobListing.svelte";
     import listings from "../listings";
+    import JobDetails from "./widgets/JobDetails.svelte";
 
     let filter = "";
 
@@ -10,14 +11,25 @@
             listing.title.toLowerCase().includes(filter.toLowerCase()) ||
             listing.org.toLowerCase().includes(filter.toLowerCase())
     );
+
+    let viewedListing;
 </script>
 
 <BasePage>
-    <h1>All Listings</h1>
-    <input bind:value={filter} placeholder="Filter..." />
-    {#each filteredListings as listing}
-        <JobListing {listing} small={true} clickCallback={() => {}} />
-    {/each}
+    {#if viewedListing}
+        <button on:click={() => (viewedListing = null)}>Back</button>
+        <JobDetails listing={viewedListing} />
+    {:else}
+        <h1>All Listings</h1>
+        <input bind:value={filter} placeholder="Filter..." />
+        {#each filteredListings as listing}
+            <JobListing
+                {listing}
+                small={true}
+                clickCallback={(l) => (viewedListing = l)}
+            />
+        {/each}
+    {/if}
 </BasePage>
 
 <style>
@@ -34,5 +46,26 @@
         background-color: #fff;
         border: 1px solid #ccc;
         margin-bottom: 24px;
+        transition: border 0.1s ease;
+    }
+
+    input:focus {
+        border: 1px solid hsl(202, 60%, 40%);
+    }
+
+    button {
+        padding: 8px 16px;
+        border: 1px solid #ccc;
+        background-color: #fff;
+        transition: background-color 0.2s ease, border 0.2s ease;
+        cursor: pointer;
+        font-size: 14px;
+        box-sizing: content-box;
+        margin-bottom: 12px;
+        transition: border 0.1s ease;
+    }
+
+    button:hover {
+        border: 1px solid hsl(202, 60%, 40%);
     }
 </style>
--- src/components/widgets/JobListing.svelte
+++ src/components/widgets/JobListing.svelte
@@ -20,9 +20,14 @@
     div {
         background-color: #fff;
         padding: 12px 16px;
-        border: 1px solid #ddd;
+        border: 1px solid #ccc;
         margin-bottom: 16px;
         user-select: none;
         cursor: pointer;
+        transition: border 0.1s ease;
+    }
+
+    div:hover {
+        border: 1px solid hsl(202, 60%, 40%);
     }
 </style>
